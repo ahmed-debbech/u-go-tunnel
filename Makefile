4cc995424467ffd341e18bf05b1de6be83a95ec9
ahmed-debbech <debbech.ahmed@gmail.com>
2025-10-03 14:39:48 +0100

    add way how to test

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: server connector all
+.PHONY: server connector all test
 
 ###### START control panel ########
 GO_BIN=/opt/go/bin/go
@@ -6,11 +6,11 @@ TERM ?= KDE  # set KDE or GNM
 ####### END control panel #########
 
 ifeq ($(TERM),GNM)
-  TERMINAL_SERV = gnome-terminal -- bash -c "cd server && $(GO_BIN) run .; exec bash"
-  TERMINAL_CLI  = gnome-terminal -- bash -c "cd connector && $(GO_BIN) run .; exec bash"
+	TERMINAL_SERV = gnome-terminal -- bash -c "cd server && $(GO_BIN) run .; exec bash"
+	TERMINAL_CLI  = gnome-terminal -- bash -c "cd connector && $(GO_BIN) run .; exec bash"
 else
-  TERMINAL_SERV = konsole --hold -e bash -c "cd server && $(GO_BIN) run ." &
-  TERMINAL_CLI  = konsole --hold -e bash -c "cd connector && $(GO_BIN) run ." &
+	TERMINAL_SERV = konsole --hold -e bash -c "cd server && $(GO_BIN) run ." &
+	TERMINAL_CLI  = konsole --hold -e bash -c "cd connector && $(GO_BIN) run ." &
 endif
 
 server:
@@ -18,5 +18,11 @@ server:
 
 connector:
 	$(TERMINAL_CLI)
+
+test:
+	@echo "==> Building binaries..."
+	cd ./connector/ && $(GO_BIN) build -o ../test/connector_bin
+	cd ./server/ && $(GO_BIN) build -o ../test/server_bin
+	@echo "==> Binaries moved to test"
 
 all: server connector
---
+++ test/test_10_sim_con.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+/*
+*	Tests the app with 10 simultinous connections
+*	with two connectors:
+*	C1 ports config: 3400
+*	C2 ports config: 3150
+ */
+const (
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorBlue   = "\033[34m"
+	ColorReset  = "\033[0m" // Resets all attributes
+)
+
+func main() {
+	log.Println("Testing 10 simultanious connections.")
+
+	cmd1 := exec.Command("./connector_bin", "", "")
+	cmd2 := exec.Command("./server_bin", "", "")
+
+	runWithPrefix("CONNECTOR", cmd1)
+	runWithPrefix("SERVER", cmd2)
+
+	select {} // block forever so both can run
+}
+
+func runWithPrefix(name string, cmd *exec.Cmd) {
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("failed to start %s: %v", name, err)
+	}
+
+	// stdout
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			fmt.Printf("%s[%s][OUT] %s%s\n", ColorGreen, name, scanner.Text(), ColorReset)
+		}
+	}()
+
+	// stderr
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			fmt.Printf("%s[%s][ERR] %s%s\n", ColorYellow, name, scanner.Text(), ColorReset)
+		}
+	}()
+
+	go func() {
+		if err := cmd.Wait(); err != nil {
+			log.Printf("[%s] exited with error: %v", name, err)
+		}
+	}()
+}
